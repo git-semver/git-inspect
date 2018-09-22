be6ed693f6ab328204f62627df99df905ee90631
Kazantsev Nickolay <kazantsev.nickolay@gmail.com>
2018-09-22 11:09:54 +0300

    Add linear history report
    Add mechanics for collect report and integration test for linear
    history report.

---
+++ integration/branch/linear-history_test.js
@@ -0,0 +1,34 @@
+const { expect }  = require('chai');
+const sinon = require('sinon');
+const map = require('lodash/map');
+const { inspect, clearGarden } = require('../helpers');
+
+describe('[Integration] Inspect history is linear', () =>
+{
+  const caseName = 'linear-history';
+  let inspector = null;
+
+  beforeEach(async () => { inspector = await inspect(caseName); });
+  afterEach(async () => await clearGarden(caseName));
+
+  it('Should be history is linear', async () =>
+  {
+    const { branch: { linear }} = await inspector.report();
+    const { isLinearHistory, branches, linearFactor } = linear;
+    const { master, topic1, topic2, topic3 } = branches;
+    expect(linearFactor).to.equal(1);
+    expect(isLinearHistory).to.equal(true);
+    expect(master.isLinearHistory).to.equal(true);
+    expect(topic1.isLinearHistory).to.equal(true);
+    expect(topic2.isLinearHistory).to.equal(true);
+    expect(topic3.isLinearHistory).to.equal(true);
+    expect(master.cousins.length).to.equal(0);
+    expect(topic1.cousins.length).to.equal(0);
+    expect(topic2.cousins.length).to.equal(0);
+    expect(topic3.cousins.length).to.equal(0);
+    expect(master.linearFactor).to.equal(1);
+    expect(topic1.linearFactor).to.equal(1);
+    expect(topic2.linearFactor).to.equal(1);
+    expect(topic3.linearFactor).to.equal(1);
+  });
+});
